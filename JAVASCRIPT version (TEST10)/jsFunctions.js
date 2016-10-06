4800aa3c6d65ca3b32044054ec7ed1425cfd08d2
wgallo3 <wkgallo3@yahoo.com>
2016-10-06 11:52:11 -0400

    Updated with Mr. Aenemic's code.... still need to refine it to be smaller though.....

--- JAVASCRIPT version (TEST10)/jsFunctions.js
+++ JAVASCRIPT version (TEST10)/jsFunctions.js
@@ -1,43 +1,54 @@
 //finds button on html page and runs auth function when clicked.
 
 document.addEventListener('DOMContentLoaded', function() {
-document.getElementById("auth_button").addEventListener("click", function(){
-authorize();
+    document.getElementById("auth_button").addEventListener("click", function(){
+    authorize();
+    
+  });
+}, false);
 
-});
-}, false);  
- 
 // stuff for auth function
-this.hostName = window.location.host.replace(/.safenet$/g, '');
-this.LOCAL_STORAGE_TOKEN_KEY = SAFE_TOKEN_${this.hostName};
-this.app = {
-name: window.location.host,
-id: 'SUCCESS!!!!!!!!'
-version: '0.0.00000001'
-vendor: 'SUCCESSFULSAFEAPPCREATOR!!'
-permissions: [
-'LOW_LEVEL_API', "SAFE_DRIVE_ACCESS"
-]
-}
-// for future dns use
-this.user = {};
+var hostName = window.location.host.replace(/.safenet$/g, '');
+var LOCAL_STORAGE_TOKEN_KEY = `SAFE_TOKEN_${hostName}`;
+var app = {
+      name: window.location.host,
+      id: 'test.auth',
+      version: '0.1.0',
+      vendor: 'chris',
+      permissions: [
+        'LOW_LEVEL_API', "SAFE_DRIVE_ACCESS"
+      ]
+    }
+
+var authToken = null;
+
+// saving token to localstorage
+function setAuthToken(token) {
+    authToken = token;
+    window.safeAuth.setAuthToken(LOCAL_STORAGE_TOKEN_KEY, token);
+  }
+
+
 
 // auth function
 function authorize() {
-console.log('Authorising application');
-console.log(hostName);
+    console.log('Authorising application');
 
-//authenticates with this.app and this.localblabla as payload
-window.safeAuth.authorise(this.app, this.LOCAL_STORAGE_TOKEN_KEY)
-  .then((res) => {
-      //success! save res.token
-      console.log(res);
-      // redirect to new page
-      window.location = "welcome.html";
-  }, (err) => {
-    // this is denied
-    console.error(err);
-    // auth failed popup
-    alert("Authentication Failed")
-  });
+    //authenticates with this.app as and this.localblabla as payload
+    window.safeAuth.authorise(app, LOCAL_STORAGE_TOKEN_KEY)
+      .then((res) => {
+          //saving token to localstorage
+           if (typeof res === 'object') {
+        setAuthToken(res.__parsedResponseBody__.token);
+      }
+          //success! save res.token
+          console.log(res);
+          // redirect to new page
+          window.location = "dnstest.html";
+      }, (err) => {
+        // this is denied
+        console.error(err);
+        // auth failed popup
+        alert("Authentication Failed")
+      });
 };
