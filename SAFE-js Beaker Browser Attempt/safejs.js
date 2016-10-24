c40687be2094e9d1508e94d4c3c06fac0111d963
wgallo3 <wkgallo3@yahoo.com>
2016-10-23 22:07:29 -0400

    added newest safe-js attempts
    also made python file .py instead of .txt

--- SAFE-js Beaker Browser Attempt/safejs.js
+++ SAFE-js Beaker Browser Attempt/safejs.js
@@ -1,9 +1,6 @@
 import * as safe from 'safe-js';
 import packageData from '../package.json'
 
-const LOCAL_STORAGE_TOKEN_KEY = 'BOOM';
-
-var token = localStorage["token"];
 // THIS JS LISTENER IS ONLY REQUIRED CUZ MAIDSAFE'S CSP HEADER'S BLOCK HTML <button onClick!!!!
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("auth_button").addEventListener("click", function(){
@@ -22,4 +19,6 @@ const app =
     permissions: ["SAFE_DRIVE_ACCESS"]
 };
 
-safe.utils.authorise( app );
+window.safeAuth.authorise( app );
+
+};
